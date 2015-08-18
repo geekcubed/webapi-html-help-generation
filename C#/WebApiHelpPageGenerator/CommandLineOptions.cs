5f08668096a6315d445604adcd1f7e9ca7a4923f
Ian Stapleton <ian@geekcubed.org>
2015-08-18 21:05:56 +0100

    Additional command-line options to specify XML help file location and an output path for generated documentation

--- C#/WebApiHelpPageGenerator/CommandLineOptions.cs
+++ C#/WebApiHelpPageGenerator/CommandLineOptions.cs
@@ -9,6 +9,12 @@ namespace WebApiHelpPageGenerator
         [Option('p', null, Required = true, HelpText = "Path to the assembly where the Web APIs are defined.")]
         public string AssemblyPath { get; set; }
 
+        [Option('d', null, Required = false, HelpText = "Path to the XML documentation file for the Web API assembly.")]
+        public string XmlDocPath { get; set; }
+
+        [Option('o', null, Required = false, HelpText = "Folder path to save generated documentation.")]
+        public string OutputPath { get; set; }
+
         [OptionArray('r', null, DefaultValue = new string[0], HelpText = "Additional assembly references.")]
         public string[] References { get; set; }
 
